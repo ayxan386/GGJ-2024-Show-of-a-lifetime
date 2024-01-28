0c71eee7a890f2e2dc4da04c2e3d22b35802e077
ayxan386 <ayxanpro@gmail.com>
2024-01-28 13:12:55 +0100

    Env almost done

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,8 +7,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
+ ;
         if ( LeaderBoard != null)
         {
           KeyManager.OnRoundEnd += EnableLeaderBoardPanel;
